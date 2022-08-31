c225bee16f39fe8cb017230a1857621acd1782bf
Anantha Kumaran <ananthakumaran@gmail.com>
2022-08-31 17:05:58 +0530

    update readme

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # OpentelemetrySentry
 
+This library provides support for propagating `sentry-trace` http
+header. This would make it possible to use distributed tracing feature
+of sentry. More details about the setup can be found in the [blog post](https://ananthakumaran.in/2022/06/11/sentry-performance-monitoring-for-elixir.html)
+
+
 ```elixir
 def deps do
   [
     {:opentelemetry_sentry, "~> 0.1.0"}
   ]
 end
+```
+
+```elixir
+config :opentelemetry,
+  text_map_propagators: [OpentelemetrySentry.Propagator]
 ```
--- mix.exs
+++ mix.exs
@@ -1,13 +1,18 @@
 defmodule OpentelemetrySentry.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/scripbox/opentelemetry_exq"
+  @version "0.1.0"
+
   def project do
     [
       app: :opentelemetry_sentry,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -19,7 +24,30 @@ defmodule OpentelemetrySentry.MixProject do
 
   defp deps do
     [
-      {:opentelemetry_api, "~> 1.0"}
+      {:opentelemetry_api, "~> 1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      description: """
+      Sentry integration for OpenTelemetry
+      """,
+      maintainers: ["Anantha Kumaran"],
+      licenses: ["MIT"],
+      files: ~w(lib test) ++ ~w(LICENSE mix.exs README.md),
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      formatters: ["html"],
+      source_url: @source_url,
+      source_ref: "v#{@version}"
     ]
   end
 end
